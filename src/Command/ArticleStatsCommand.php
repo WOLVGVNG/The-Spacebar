516e3e7bb7348a6dd69984d6f8d06ed139e9a78d
Jacek Obst <j.obst@webwizards.pl>
2020-02-18 13:16:05 +0000

    Add maker composer package and init the ArticleStatsCommand

---
+++ src/Command/ArticleStatsCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class ArticleStatsCommand extends Command
+{
+    protected static $defaultName = 'article:stats';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Returns some article stats')
+            ->addArgument('slug', InputArgument::REQUIRED, 'The Article\'s slug')
+            ->addOption('format', null, InputOption::VALUE_REQUIRED, 'The output format', 'text')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return 0;
+    }
+}
