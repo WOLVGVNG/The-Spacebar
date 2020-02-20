e009c2deafca16981c69008e0afd39d867130dc0
Jacek Obst <j.obst@webwizards.pl>
2020-02-20 10:00:51 +0000

    Add timestamps to Article

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -53,6 +53,16 @@ class Article
      */
     private $imageFilename;
 
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -151,5 +161,29 @@ class Article
 
     public function getImagePath() {
         return 'images/'.$this->getImageFilename();
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
     }
 }
