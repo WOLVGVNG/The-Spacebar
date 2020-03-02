7e16da61f3fc20e9045c3bee1012bcec27f6214d
Jacek Obst <j.obst@webwizards.pl>
2020-03-02 16:07:01 +0000

    Optimize ManyToMany relation queries by adding the JOINS

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -38,6 +38,8 @@ class ArticleRepository extends ServiceEntityRepository
             ->addCriteria(self::createNonDeletedCriteria());
 
         return $this->addIsPublishedQueryBuilder()
+            ->leftJoin('a.tags', 't')
+            ->addSelect('t')
             ->orderBy('a.publishedAt', 'DESC')
             ->getQuery()
             ->getResult();
